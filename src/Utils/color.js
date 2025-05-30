2f52b034cbb1166720d74254cf7931b053f1a7dc
Mynameajit <132321923+Mynameajit@users.noreply.github.com>
2025-05-30 12:54:14 +0530

    Delete src/Utils/color.js

--- src/Utils/color.js
+++
@@ -1,3 +0,0 @@
-export const buttonBgColor="linear-gradient(to right, #2600d9, #ff00bf)" 
-export const headerBg="linear-gradient(to right, #2600d9, #ff00bf)" 
-export const BtnBg="linear-gradient(to right, #ff0040, #0000ff)" 
